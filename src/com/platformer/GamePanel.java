5077c44c308b6a70a9d08acf3be9e4adc2d2a201
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-08-02 08:34:07 -0500

    part1

--- src/com/platformer/GamePanel.java
+++ src/com/platformer/GamePanel.java
@@ -99,6 +99,9 @@ implements Runnable, KeyListener {
 
             wait = targetTime
                     - elapsed /1000000;
+
+            if(wait < 0) wait = 5;
+
             try{
                 Thread.sleep(wait);
             }catch(Exception e){
